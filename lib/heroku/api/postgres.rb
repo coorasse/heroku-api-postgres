3cd9345a456afa58a521296af4bc0014a222d7fb
Vokhmin Aleksei V <avokhmin@gmail.com>
2022-11-23 22:44:23 +0200

    [#14] Allow to rotate DB credentials (#14)

--- lib/heroku/api/postgres.rb
+++ lib/heroku/api/postgres.rb
@@ -2,5 +2,6 @@
 
 require 'heroku/api/postgres/version'
 require 'heroku/api/postgres/client'
+require 'heroku/api/postgres/credentials'
 require 'heroku/api/postgres/backups'
 require 'heroku/api/postgres/databases'
